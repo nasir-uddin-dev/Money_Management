c8d01636b874b9dfc2415e8b99c6b4b572156cb9
Nasir Uddin <nasiruddin3954@gmail.com>
2025-08-19 09:54:34 +0600

    money

---
+++ lib/Money_Management/money_management.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class MoneyManagement extends StatefulWidget {
+  const MoneyManagement({super.key});
+
+  @override
+  State<MoneyManagement> createState() => _MoneyManagementState();
+}
+
+class _MoneyManagementState extends State<MoneyManagement> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
